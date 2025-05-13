09a67e4327f3e1941e74d0e51927e4aa917885fd
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-13 18:54:14 +0700

    Membuat Definisi constructor dengan parameter

--- Constructorberparameter.cpp
+++ Constructorberparameter.cpp
@@ -7,3 +7,10 @@ class mahasiswa {
     // Constructor dengan parameter
     mahasiswa(int nim, string nama);
 };
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM   : " << nim << endl;
+    cout << "Nama  : " << nama << endl;
+}
