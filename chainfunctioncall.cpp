cc4fdd55f1982bb18f19f5228087a12dc9580c55
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-13 19:32:38 +0700

    membuat int main dan membuat objek dengan nama "bukunya"

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 class buku {
-
+    string judul;
+    public:
+    buku setJudul(string judul){
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul(){
+        return this->judul;
+    }
 };
+
+int main (){
+    buku bukunya;
+    // chain function calls
+    cout << bukunya.setJudul("Matematika").getJudul();
+    return 0;
+}
