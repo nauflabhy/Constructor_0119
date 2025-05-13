663aba8f5d0c2e52c1b7f4e725a89726f05083ad
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-13 19:22:00 +0700

    Membuat class buku akses public dan memasukan fungsi "this"

--- this.cpp
+++ this.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 class buku {
-
+    string judul;
+    public:
+        string setget(string jdl)
+        {
+            // Nilai parameter "judul" untuk member variable "judul"
+            this->judul = jdl;
+            // Return variable judul
+            return this->judul;
+        }
 };
